d1b91844129173225f0f049f2f4b09f226b213fb
huzaifa-anw <anwarhuzaifa226@gmail.com>
2025-10-13 18:58:18 +0500

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import MovieCard from './components/MovieCard'
+import { useEffect, useState } from 'react';
+
+function App() {
+
+    const configURL = 'https://api.themoviedb.org/3/configuration';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`
+        }
+    };
+
+    // state for storing the base url for accessing images
+    const [imageBaseURL, setImageBaseURL] = useState('');
+
+    useEffect(() => {
+        async function fetchConfig() {
+            let response = await fetch(configURL, options);
+            let data = await response.json();
+            setImageBaseURL(data.images.base_url);
+        }
+        fetchConfig();
+    }, [])
+
+  return (
+    <>
+      <h1>cineflix</h1>
+      <p>black purple dark purple</p>
+      <h2>trending movies: </h2>
+      <MovieCard  />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,8 @@
+export default function MovieCard () {
+    return(
+        <div className="movie-card">
+            <p>movie title</p>
+            rating: 5
+        </div>
+    );
+}
