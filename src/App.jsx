e19c8cc0862b981d0601ab562a3135e3f742aa38
huzaifa-anw <anwarhuzaifa226@gmail.com>
2025-11-18 00:48:39 +0500

    added states for loading

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,11 @@ function App() {
 
     // state for storing terending movies
     const [ trendingMovies, setTrendingMovies ] = useState([]);
+    const [ isTMLoaded, setIsTMLoaded ] = useState(false);
 
     // TODO state for storing now-playing movies
     const [ nowPlayingMovies, setNowPlayingMovies] = useState([]);
+    const [ isNPMloaded, setIsNPMLoaded ] = useState(false);
 
     // get config details (base image url)
     useEffect(() => {
@@ -39,6 +41,7 @@ function App() {
             let response = await fetch('https://api.themoviedb.org/3/trending/movie/week', options);
             let data = await response.json();
             setTrendingMovies(data.results.slice(0,10));
+            setIsTMLoaded(true);
         }
         getTrendingMovies();
     }, [])    
@@ -49,6 +52,7 @@ function App() {
             let response = await fetch('https://api.themoviedb.org/3/movie/now_playing', options);
             let data = await response.json();
             setNowPlayingMovies(data.results);
+            setIsNPMLoaded(true);
         }
         getNowPlayingMovies();
     }, [])
@@ -65,12 +69,20 @@ function App() {
         <h1>Trending &gt;&gt;</h1>
         <div className="trending-movies-container">
             {
+                isTMLoaded ?
                 trendingMovies.map(trendingMovie => {
                     return <MovieCard 
                         key={trendingMovie.id}
                         title={trendingMovie.original_title} 
                         imageBaseURL={imageBaseURL}
                         posterPath={trendingMovie.poster_path}
+                        isTMLoaded={isTMLoaded}
+                    />
+                }) :
+                Array.from({length: 10}).map((v, i) => {
+                    return <MovieCard 
+                        key={i}
+                        isTMLoaded={isTMLoaded}
                     />
                 })
             }
